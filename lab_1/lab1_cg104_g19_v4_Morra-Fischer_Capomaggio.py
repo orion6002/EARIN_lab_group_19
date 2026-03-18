4d88f80651a8b9f660385ccd54d6c4302e490b5d
orion6002 <capomaggio.nolan@gmail.com>
2026-03-18 14:01:53 +0100

    Main alg written, only miss Hessian Matrix and ak missing

--- lab_1/lab1_cg104_g19_v4_Morra-Fischer_Capomaggio.py
+++ lab_1/lab1_cg104_g19_v4_Morra-Fischer_Capomaggio.py
@@ -6,7 +6,6 @@ from matplotlib import cm
 def function(x, y):
     return 2 * np.sin(x) + 3 * np.cos(y)
 
-# 2cosx + 3
 def newton_method(initial_guess, alpha, tol=1e-6, max_iter=1000):
     """
     Newton method
@@ -16,13 +15,23 @@ def newton_method(initial_guess, alpha, tol=1e-6, max_iter=1000):
     - alpha: step size parameter
     - tol: tolerance, convergence criteria
     - max_iter: maximum number of iterations
-
     """
 
     dx, dy = 0
+    Hf = [[0, 0], [0, 0]]
+    dk = [0, 0]
+    ak = 0
+
     current_guess = initial_guess
-    for i in range (1000):
+    for i in range (max_iter):
         dx, dy = np.gradient(function(current_guess[0], current_guess[1]), current_guess[0], current_guess[1])
+        if np.sqrt(dx**2 + dy**2) > tol :
+            #generate Hessian Matrix [[][]] based on the current guess
+            dk = -1 * np.matmul(Hf.np.linalg.inv(), [dx, dy])
+            current_guess[0], current_guess[1] = current_guess[0] + ak * dk[0], current_guess[1] + ak * dk[1]
+        else:
+            return current_guess
+    return current_guess
 
 
 def visualize():
