dd862387c79923f9c131767d2b47d878776fc4bc
orion6002 <capomaggio.nolan@gmail.com>
2026-03-18 12:40:41 +0100

    First_changes

---
+++ lab_1/lab1_v4.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+def function(x, y):
+    return 2*np.sin(x)+3*np.cos(y)
+
+def newton_method(initial_guess, alpha, tol=1e-6, max_iter=1000):
+    """
+    Newton method
+    
+    Parameters:
+    - initial_guess: initial 2D coordinate vector
+    - alpha: step size parameter
+    - tol: tolerance, convergence criteria
+    - max_iter: maximum number of iterations
+
+    """
+
+def visualize():
+    """
+    Visualization function: creates 3D plot of the function. Use colors to show the Z-coordinate
+    """
+
+visualize()
+
+#Example usage:
+initial_guess_1 = [2.0, 2.0]
+learning_rate_1 = 0.1
+minimum_1, iterations_1 = newton_method(initial_guess_1, learning_rate_1)
+
+
+print(f"Minimum approximation with initial guess {initial_guess_1}: {minimum_1}, Iterations: {iterations_1}")
