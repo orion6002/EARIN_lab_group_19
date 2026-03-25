b19d7821d63b1489fd1ce8145564c874b7a79c7c
orion6002 <capomaggio.nolan@gmail.com>
2026-03-25 12:35:22 +0100

    changes to the fnc

--- lab_1/lab1_cg104_g19_v4_Morra-Fischer_Capomaggio.py
+++ lab_1/lab1_cg104_g19_v4_Morra-Fischer_Capomaggio.py
@@ -1,3 +1,6 @@
+from ast import If
+
+from anyio import current_effective_deadline
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -5,6 +8,7 @@ from matplotlib import cm
 
 def function(x, y):
     return 2 * np.sin(x) + 3 * np.cos(y)
+    # z = 2.sin(x) + 3.cos(y)
 
 def newton_method(initial_guess, alpha, tol=1e-6, max_iter=1000):
     """
@@ -17,21 +21,25 @@ def newton_method(initial_guess, alpha, tol=1e-6, max_iter=1000):
     - max_iter: maximum number of iterations
     """
 
-    dx, dy = 0
-    Hf = [[0, 0], [0, 0]]
+    dx, dy = 0, 0
+    Hf_inv = [[0, 0], [0, 0]]
     dk = [0, 0]
-    ak = 0
 
     current_guess = initial_guess
     for i in range (max_iter):
-        dx, dy = np.gradient(function(current_guess[0], current_guess[1]), current_guess[0], current_guess[1])
+        dx = 2 * np.cos(current_guess[0])
+        dy = -3 * np.sin(current_guess[1])
         if np.sqrt(dx**2 + dy**2) > tol :
-            #generate Hessian Matrix [[][]] based on the current guess
-            dk = -1 * np.matmul(Hf.np.linalg.inv(), [dx, dy])
-            current_guess[0], current_guess[1] = current_guess[0] + ak * dk[0], current_guess[1] + ak * dk[1]
+            Hf_inv = np.linalg.inv([[-2*np.sin(dx), 0], [0, -3*np.cos(dy)]])
+            dk = -1 * np.matmul(Hf_inv, [dx, dy])
+            # we have to check if the current_guess is not out of bounds
+            if abs(current_guess[0] + alpha * dk[0]) > max_abs_range[0] or abs(current_guess[1] + alpha * dk[1]) > max_abs_range[1] :
+                current_guess[0], current_guess[1] = (current_guess[0] + alpha * dk[0]), (current_guess[1] + alpha * dk[1])
+                return (current_guess, i+1)
+            current_guess[0], current_guess[1] = current_guess[0] + alpha * dk[0], current_guess[1] + alpha * dk[1]
         else:
-            return current_guess
-    return current_guess
+            return (current_guess, i+1)
+    return (current_guess, max_iter)
 
 
 def visualize():
@@ -40,10 +48,12 @@ def visualize():
     """
 
 
-visualize()
+# visualize()
 
 # Example usage:
+max_abs_range = [5.5, 5.5]
 initial_guess_1 = [2.0, 2.0]
+print(f"test {initial_guess_1} test\n")
 learning_rate_1 = 0.1
 minimum_1, iterations_1 = newton_method(initial_guess_1, learning_rate_1)
 
