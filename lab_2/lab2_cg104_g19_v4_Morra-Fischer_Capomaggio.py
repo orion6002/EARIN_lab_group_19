8702e15124a0b989fe91916169bdbe95e3c1e6c7
Mathis <87679380+Mathiiis@users.noreply.github.com>
2026-04-06 18:36:55 +0200

    Backtrack and consistency check

--- lab_2/lab2_cg104_g19_v4_Morra-Fischer_Capomaggio.py
+++ lab_2/lab2_cg104_g19_v4_Morra-Fischer_Capomaggio.py
@@ -36,7 +36,7 @@ class CSP:
 
 	def solve(self): 
 		assignment = {} 
-		self.solution = self.backtrack(assignment) 
+		self.solution = self.backtrack(assignment, self.domains) 
 		return self.solution 
 	
 	def forward_checking(self, var, value, assignment, domains_copy):
@@ -57,8 +57,15 @@ class CSP:
 				if len(domains_copy[neighbor]) == 0:
 					return False
 		return True
+	
+	# Verify if the value is not already assigned to a neighbor
+	def is_consistent(self, var, value, assignment):
+		for neighbor in self.constraints[var]:
+			if neighbor in assignment and assignment[neighbor] == value:
+				return False
+		return True
 	  
-	def backtrack(self, assignment): 
+	def backtrack(self, assignment, domains): 
 		"""
 		Backtracking algorithm
 
@@ -68,8 +75,37 @@ class CSP:
 		Returns:
 		- assignment: dict with all the assigments to the variables, or None if solution is not found. Return the first found solution
 		"""
+		# Verify if every var are already assigned
+		if len(assignment) == len(self.variables):
+			return assignment
+		
+		# Chose a non assigned var 
+		for var in self.variables:
+			if var not in assignment:
+				current_var = var
+				break
+		
+		# Try every values in its domain
+		for value in domains[current_var]:
 
+			if self.is_consistent(current_var, value, assignment):
+				assignment[current_var] = value
+				
+				domains_copy = {v: domains[v][:] for v in domains}
 
+				domains_copy[current_var] = [value]
+
+				# Update domains and constraints
+				if self.forward_checking(current_var, value, assignment, domains_copy):
+	
+					# Continue recursivly to find a solution in this path
+					result = self.backtrack(assignment, domains_copy)
+					if result is not None:
+						return result
+					
+				del assignment[current_var]
+			
+		return None
 
 puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0], 
 		  [0, 0, 0, 1, 0, 5, 0, 0, 0], 
